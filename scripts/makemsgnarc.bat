8b846ed28e7987c1463fa454aa50022c81fc3164
Nathan Wichers <bluroseai@hotmail.com>
2021-01-05 00:35:17 -0500

    bam victini

--- scripts/makemsgnarc.bat
+++ scripts/makemsgnarc.bat
@@ -1,4 +1,8 @@
 cd text
-thenewpoketext < script.txt
+thenewpoketext < script1.txt
+cd ..\
+xcopy "rawdata\a027withalotofblanks" "text\tmp_base\root\a\0\2\7"
+cd text
+thenewpoketext < script2.txt
 cd ..\
 xcopy "text\tmp_base\a027.narc" "a027.narc"
