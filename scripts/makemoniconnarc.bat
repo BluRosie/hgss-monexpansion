fcdc7c491247528218d8c316acdea959e7828669
Nathan Wichers <bluroseai@hotmail.com>
2021-04-02 00:09:00 -0400

    rip all the icons directly from gen 5
    had to rerip all of them to fit the new palette ahaha
    
    also fix the commas in the height table

--- scripts/makemoniconnarc.bat
+++ scripts/makemoniconnarc.bat
@@ -1,6 +1,6 @@
 mkdir a020
 xcopy "rawdata\first files from a020\*" "a020"
-tools\geniconnarc.exe graphics\icongfx a020 545
+tools\geniconnarc.exe graphics\icongfx a020 699
 tools\narchive create a020.narc a020 -nf
 rmdir /q /s a020
 tools\armips data\iconpalettetable.s
