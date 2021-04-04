4c2c57426f7d97b46473f02ce45d85916b8b4134
Nathan Wichers <bluroseai@hotmail.com>
2021-04-04 04:09:57 -0400

    makeoverworldnarc specific to vanilla

--- scripts/makeoverworldnarc.bat
+++ scripts/makeoverworldnarc.bat
@@ -1,10 +1,10 @@
 tools\narchive extract a081.narc -o a081 
 for /l %%F in (297, 1, 999) do (
  if exist "graphics\overworlds\%%F.png" (
-  tools\pngtobtx0 graphics\overworlds\%%F.png ))
+  tools\pngtobtx0 graphics\overworlds\%%F.png a081\a081_%%F ))
 for /l %%F in (1000, 1, 1500) do (
  if exist "graphics\overworlds\a%%F.png" (
-  tools\pngtobtx0 graphics\overworlds\%%F.png ))
+  tools\pngtobtx0 graphics\overworlds\%%F.png a081\b081_%%F ))
 tools\narchive create a081.narc a081 -nf
 rmdir /q /s a081
 mkdir a141
