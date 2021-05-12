4a0f35104094d9512439b97cf61c0878d2d60cfc
Nathan Wichers <bluroseai@hotmail.com>
2021-05-12 03:29:16 -0400

    dex data - body type and weight lists
    now need all the alphabetical lists, the height lists, and the type lists

--- data/pokedex/sortlists/bodytypehead.s
+++ data/pokedex/sortlists/bodytypehead.s
@@ -4,12 +4,12 @@
 .create "a214/091.bin", 0x0
 
 .halfword SPECIES_SHELLDER
-.halfword SPECIES_CLOYSTER
 .halfword SPECIES_GASTLY
 .halfword SPECIES_VOLTORB
-.halfword SPECIES_ELECTRODE
 .halfword SPECIES_KOFFING
 .halfword SPECIES_DITTO
+.halfword SPECIES_CLOYSTER
+.halfword SPECIES_ELECTRODE
 .halfword SPECIES_SUNKERN
 .halfword SPECIES_MISDREAVUS
 .halfword SPECIES_UNOWN
@@ -21,11 +21,30 @@
 .halfword SPECIES_SOLROCK
 .halfword SPECIES_CASTFORM
 .halfword SPECIES_SHUPPET
-.halfword SPECIES_GLALIE
 .halfword SPECIES_CLAMPERL
-.halfword SPECIES_MISMAGIUS
+.halfword SPECIES_GLALIE
 .halfword SPECIES_BRONZOR
+.halfword SPECIES_MISMAGIUS
 .halfword SPECIES_ROTOM
+.halfword SPECIES_COTTONEE
+.halfword SPECIES_SOLOSIS
+.halfword SPECIES_FERROSEED
+.halfword SPECIES_WHIRLIPEDE
+.halfword SPECIES_DUOSION
+.halfword SPECIES_CRYOGONAL
+.halfword SPECIES_SHELMET
+//.halfword SPECIES_CARBINK
+//.halfword SPECIES_KLEFKI
+//.halfword SPECIES_PUMPKABOO
+//.halfword SPECIES_COMFEY
+//.halfword SPECIES_MINIOR
+//.halfword SPECIES_COSMOG
+//.halfword SPECIES_MELTAN
+//.halfword SPECIES_ROLYCOLY
+//.halfword SPECIES_COSMOEM
+//.halfword SPECIES_SINISTEA
+//.halfword SPECIES_MILCERY
+//.halfword SPECIES_POLTEAGEIST
 
 
 .close
